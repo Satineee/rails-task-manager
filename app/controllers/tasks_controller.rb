3e2db27e04697511f766501098c70f3380f1856d
satine fouque <satine.fouque@gmail.com>
2021-07-27 15:17:58 +0400

    update ajouté

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,37 @@
+class TasksController < ApplicationController
+  def list
+    @list = Task.all
+  end
+
+  def show
+    @detail = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    flash[:notice] = "la task a bien été créé"
+    redirect_to task_path(@task)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    flash[:notice] = "la task a bien été mise à jour"
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
