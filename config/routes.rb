8b3e977999b417f27e0309a608dc78ba30f6644b
satine fouque <satine.fouque@gmail.com>
2021-07-27 15:52:49 +0400

    delete reussi

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#list'
   post '/tasks', to: 'tasks#create'
   get '/tasks/new', to: 'tasks#new', as: :new_task
-  get 'tasks/:id/edit', to: 'tasks#edit',   as: :edit_task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   get '/tasks/:id', to: 'tasks#show', as: :task
   patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
